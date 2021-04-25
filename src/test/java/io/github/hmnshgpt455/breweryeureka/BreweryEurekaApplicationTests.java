8f184e95612800ae7cc503341f9c2bd456000e81
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-25 20:48:58 +0530

    Adding initial project. Closes #1.

---
+++ src/test/java/io/github/hmnshgpt455/breweryeureka/BreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hmnshgpt455.breweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
